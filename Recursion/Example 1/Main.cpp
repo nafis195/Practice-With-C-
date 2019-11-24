a5d418e5fe5242d0d25c5b590768612a1ec29cd0
Nafis Khan Chowdhury <nafisbuet1@gmail.com>
2019-11-23 21:20:09 -0800

    Add files via upload

--- Recursion/Example 1/Main.cpp
+++ Recursion/Example 1/Main.cpp
@@ -1,1 +1,40 @@
+// Bismillahir Rahmanir Rahim
+// Nafis Chowdhury
+// Recursion Example #1
+// http://zobayer.blogspot.com/2009/12/cse-102-practice-recursions.html
 
+
+#include<iostream>
+using namespace std;
+
+// function prototype
+void print(int *A, int n, int i);
+
+// main function
+int main()
+{
+	int n, i, A[10];
+	cout << "Please input how many number:- " << endl;
+	cin >> n;
+
+	for (i = 0; i < n; i++)
+		cin >> A[i];
+
+	print(A, n, 0);  // function call
+
+	cout << endl;
+	system("pause");
+	return 0;
+}
+
+// print function
+void print(int *A, int n, int i)
+{
+	if (i < n)
+	{
+		print(A, n, i + 1);
+		cout << A[i] << ' ';
+	}
+	else
+		return;
+}
