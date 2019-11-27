fb448bf2bfe4dcb1ddfdc1d85ba678a6c357c064
Nafis Khan Chowdhury <nafisbuet1@gmail.com>
2019-11-26 20:26:21 -0800

    Add files via upload

--- Recursion/Example 6/Main.cpp
+++ Recursion/Example 6/Main.cpp
@@ -1,1 +1,36 @@
+// Bismillahir Rahmanir Rahim
+// Nafis Chowdhury
+// Recursion Example #6
+// Problem statement- http://zobayer.blogspot.com/2009/12/cse-102-practice-recursions.html
+// Main.cpp
 
+
+#include<iostream>
+using namespace std;
+
+// function template
+int factorial(int i);
+
+// main function
+int main()
+{
+	int i, answer;
+	cout << "Please enter a number:- ";
+	cin >> i;
+
+	answer = factorial(i);  // function call
+	cout << "Factorial is:- " << answer;
+
+	cout << endl;
+	system("pause");
+	return 0;
+}
+
+// factorial function
+int factorial(int i)
+{
+	if (i == 0 || i == 1)
+		return 1;
+	else
+		return i * factorial(i - 1);
+}
