e25bf0d3726732e9c91ea066f3d27676d6949b0c
Nafis Khan Chowdhury <nafisbuet1@gmail.com>
2019-11-26 20:14:02 -0800

    Add files via upload

--- Recursion/Example 5/Main.cpp
+++ Recursion/Example 5/Main.cpp
@@ -1,1 +1,45 @@
+// Bismillahir Rahmanir Rahim
+// Nafis Chowdhury
+// Recursion Example #5
+// Problem statement- http://zobayer.blogspot.com/2009/12/cse-102-practice-recursions.html
+// Mian.cpp
 
+
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+// function template
+int polynomial_print(int i, int n, int j);
+
+// global variable to keep track of the sum of the series
+int sum = 0;
+
+// main function
+int main()
+{
+	int i, j, n;
+	cout << "Please input the value of X:- ";
+	cin >> i;
+	cout << endl;
+	cout << "Please input another number:- ";
+	cin >> n;
+
+	int answer = polynomial_print(i, n, 0);  // function call
+	cout << "Sum is:- " << answer << endl;
+
+	cout << endl;
+	system("pause");
+	return 0;
+}
+
+// polynomial_print function
+int polynomial_print(int i, int n, int j)
+{
+	if (j < n)
+	{
+		polynomial_print(i, n, j + 1);
+		sum = sum + pow(i, j);
+	}
+	return sum;
+}
