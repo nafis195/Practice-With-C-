5983a3058cf19c69afdd68412330e067044b0751
Nafis Khan Chowdhury <nafisbuet1@gmail.com>
2020-01-20 00:33:56 -0800

    Add files via upload

--- Pointer Array/Main.cpp
+++ Pointer Array/Main.cpp
@@ -1,1 +1,41 @@
+// Bismillahir Rahmanir Rahim
+// Pointer Array
+// Main.cpp
 
+
+#include<iostream>
+#include<string>
+#include<algorithm>
+using namespace std;
+
+struct item
+{
+	int id;
+	string name;
+	item* next;
+};
+
+int main()
+{
+	int idNum;
+	string stName;
+	item* pointerArray[3];
+
+	for(int i = 0; i <= 2; i++)
+	{
+		pointerArray[i] = new item();
+		cin >> idNum;
+		pointerArray[i]->id = idNum;
+		cin >> stName;
+		pointerArray[i]->name = stName;
+		pointerArray[i]->next = NULL;
+	}
+
+	for(int i = 0; i <= 2; i++)
+	{
+		cout << pointerArray[i]->id << ' ' << pointerArray[i]->name << endl;
+	}
+
+	system("pause");
+	return 0;
+}
